64730c21dc382959bf9b76b87b0d23d27e5d4115
LittleCai <2243157861@qq.com>
2020-07-05 23:40:27 +0800

    Third Commit

--- G_Manage.Designer.cs
+++ G_Manage.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -39,12 +38,12 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -56,19 +55,10 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "家谱ID";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(353, 110);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 18);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "家谱信息如下";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(60, 155);
+            this.label3.Location = new System.Drawing.Point(60, 107);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(80, 18);
             this.label3.TabIndex = 2;
@@ -77,7 +67,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(395, 154);
+            this.label4.Location = new System.Drawing.Point(395, 106);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(80, 18);
             this.label4.TabIndex = 3;
@@ -86,7 +76,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(63, 208);
+            this.label5.Location = new System.Drawing.Point(63, 160);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(80, 18);
             this.label5.TabIndex = 4;
@@ -95,7 +85,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(66, 258);
+            this.label6.Location = new System.Drawing.Point(65, 206);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(44, 18);
             this.label6.TabIndex = 5;
@@ -104,7 +94,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(69, 303);
+            this.label7.Location = new System.Drawing.Point(56, 279);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(80, 18);
             this.label7.TabIndex = 6;
@@ -112,58 +102,45 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(180, 35);
+            this.textBox1.Location = new System.Drawing.Point(178, 43);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(502, 28);
+            this.textBox1.Size = new System.Drawing.Size(379, 28);
             this.textBox1.TabIndex = 7;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(178, 143);
+            this.textBox2.Location = new System.Drawing.Point(178, 95);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(176, 28);
             this.textBox2.TabIndex = 8;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(520, 144);
+            this.textBox3.Location = new System.Drawing.Point(520, 96);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(160, 28);
             this.textBox3.TabIndex = 9;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(178, 197);
+            this.textBox4.Location = new System.Drawing.Point(178, 149);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(502, 28);
             this.textBox4.TabIndex = 10;
             // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(178, 247);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(502, 28);
-            this.textBox5.TabIndex = 11;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(178, 292);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(502, 28);
-            this.textBox6.TabIndex = 12;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(370, 69);
+            this.button1.Location = new System.Drawing.Point(597, 46);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 27);
             this.button1.TabIndex = 13;
             this.button1.Text = "确认";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(68, 351);
+            this.button2.Location = new System.Drawing.Point(74, 382);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(88, 44);
             this.button2.TabIndex = 14;
@@ -173,16 +150,17 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(381, 350);
+            this.button3.Location = new System.Drawing.Point(373, 382);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(88, 44);
             this.button3.TabIndex = 15;
             this.button3.Text = "一键删除";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(597, 351);
+            this.button4.Location = new System.Drawing.Point(603, 382);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(88, 44);
             this.button4.TabIndex = 16;
@@ -190,17 +168,33 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(175, 203);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(505, 55);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(178, 279);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(499, 71);
+            this.richTextBox2.TabIndex = 18;
+            this.richTextBox2.Text = "";
+            // 
             // G_Manage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -210,7 +204,6 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "G_Manage";
             this.Text = "G_Manage";
@@ -222,7 +215,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
@@ -232,11 +224,11 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
     }
 }
--- G_Manage.cs
+++ G_Manage.cs
@@ -28,7 +28,99 @@ namespace Genealogy_Management_System
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+            string _Fname = textBox2.Text.Trim();//族谱ID
+            string constr = "Server=.;Database=Genealogy Management System;Integrated Security=True";
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+
+            string sql;
+
+            //判断族谱ID是否存在
+            SqlCommand com1 = new SqlCommand("Select G_ID from Genealogy where G_ID='" + _Fname + "'", con);
+            SqlDataAdapter da1 = new SqlDataAdapter(com1);
+            DataSet ds1 = new DataSet();
+            int n1 = da1.Fill(ds1, "Genealogy");
+
+
+
+            if (n1 > 0)
+            {
+                sql = "update Genealogy set G_Name ='" + textBox2.Text + "',G_Surname='" + textBox3.Text + "',G_GenerationIntro='" + textBox4.Text + "',G_Educ='" + richTextBox1.Text + "',G_Intro='" + richTextBox2.Text + "'where G_ID='" + textBox1.Text + "'";
+                com1 = new SqlCommand(sql, con);
+
+                try
+                {
+                    com1.ExecuteNonQuery();
+                    MessageBox.Show("信息修改成功！");
+                }
+                catch (Exception msg)
+                {
+                    MessageBox.Show("修改失败！\n出错原因：" + msg.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("族谱ID不存在！", "提示");
+                textBox2.Text = "";
+            }
+            com1 = null;
+            con.Close();
+        }
+        //查询
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text.Trim();//家谱ID
+            //string _Fname = textBox3.Text.Trim();//族谱ID
+            string constr = "Server=.;Database=Genealogy Management System;Integrated Security=True";
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+
+            string sql = "select * from Genealogy where G_ID='" + name + "'";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            //判断事件ID是否存在
+            if (!dr.Read())
+            {
+                MessageBox.Show("不存在该事件");
+                textBox1.Text = "";
+            }
+
+            else
+            {
+                //显示相应事件信息
+                textBox2.Text = dr["G_Name"].ToString();
+                textBox3.Text = dr["G_Surname"].ToString();
+                textBox4.Text = dr["G_GenerationIntro"].ToString();
+                richTextBox1.Text = dr["G_Educ"].ToString();
+                richTextBox2.Text = dr["G_Intro"].ToString();
+
+
+            }
+            dr.Close();
+            con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string constr = "Server=.;Database=Genealogy Management System;Integrated Security=True";
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+
+            string sql = "delete from Genealogy  where G_ID='" + textBox1.Text + "'";
+
+            SqlCommand com1 = new SqlCommand(sql, con);
+
+            try
+            {
+                com1.ExecuteNonQuery();
+                MessageBox.Show("信息修改成功！");
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show("修改失败！\n出错原因：" + msg.Message);
+            }
+
+            con.Close();
         }
     }
 }
