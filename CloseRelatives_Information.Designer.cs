4b060b03650bdff2551f546c38d27a29a168e828
LittleCai <2243157861@qq.com>
2020-07-06 15:45:57 +0800

    Six Commit Version

--- CloseRelatives_Information.Designer.cs
+++ CloseRelatives_Information.Designer.cs
@@ -41,7 +41,6 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -59,7 +58,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 19);
+            this.label1.Location = new System.Drawing.Point(90, 62);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(116, 18);
             this.label1.TabIndex = 0;
@@ -67,17 +66,18 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(50, 54);
+            this.button1.Location = new System.Drawing.Point(710, 51);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(61, 28);
+            this.button1.Size = new System.Drawing.Size(76, 40);
             this.button1.TabIndex = 1;
             this.button1.Text = "查询";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(59, 105);
+            this.label2.Location = new System.Drawing.Point(102, 148);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 18);
             this.label2.TabIndex = 2;
@@ -87,7 +87,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(455, 104);
+            this.label3.Location = new System.Drawing.Point(498, 147);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 18);
             this.label3.TabIndex = 3;
@@ -96,7 +96,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(62, 148);
+            this.label4.Location = new System.Drawing.Point(105, 191);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 18);
             this.label4.TabIndex = 4;
@@ -105,7 +105,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(458, 147);
+            this.label5.Location = new System.Drawing.Point(501, 190);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(44, 18);
             this.label5.TabIndex = 5;
@@ -114,7 +114,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(62, 192);
+            this.label6.Location = new System.Drawing.Point(105, 235);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(44, 18);
             this.label6.TabIndex = 6;
@@ -123,7 +123,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(458, 191);
+            this.label7.Location = new System.Drawing.Point(501, 234);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(44, 18);
             this.label7.TabIndex = 7;
@@ -132,7 +132,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(62, 234);
+            this.label8.Location = new System.Drawing.Point(105, 277);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(80, 18);
             this.label8.TabIndex = 8;
@@ -141,7 +141,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(461, 234);
+            this.label9.Location = new System.Drawing.Point(504, 277);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(44, 18);
             this.label9.TabIndex = 9;
@@ -150,7 +150,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(59, 281);
+            this.label10.Location = new System.Drawing.Point(102, 324);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(80, 18);
             this.label10.TabIndex = 10;
@@ -159,94 +159,85 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(458, 281);
+            this.label11.Location = new System.Drawing.Point(501, 324);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(62, 18);
             this.label11.TabIndex = 11;
             this.label11.Text = "出生地";
             // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(62, 325);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(44, 18);
-            this.label12.TabIndex = 12;
-            this.label12.Text = "子女";
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(184, 13);
+            this.textBox1.Location = new System.Drawing.Point(227, 56);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(559, 28);
+            this.textBox1.Size = new System.Drawing.Size(459, 28);
             this.textBox1.TabIndex = 14;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(153, 94);
+            this.textBox2.Location = new System.Drawing.Point(196, 137);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(240, 28);
             this.textBox2.TabIndex = 15;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(550, 94);
+            this.textBox3.Location = new System.Drawing.Point(593, 137);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(193, 28);
             this.textBox3.TabIndex = 16;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(153, 137);
+            this.textBox4.Location = new System.Drawing.Point(196, 180);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(240, 28);
             this.textBox4.TabIndex = 17;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(550, 137);
+            this.textBox5.Location = new System.Drawing.Point(593, 180);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(193, 28);
             this.textBox5.TabIndex = 18;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(153, 181);
+            this.textBox6.Location = new System.Drawing.Point(196, 224);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(240, 28);
             this.textBox6.TabIndex = 19;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(550, 181);
+            this.textBox7.Location = new System.Drawing.Point(593, 224);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(193, 28);
             this.textBox7.TabIndex = 20;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(153, 223);
+            this.textBox8.Location = new System.Drawing.Point(196, 266);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(240, 28);
             this.textBox8.TabIndex = 21;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(550, 223);
+            this.textBox9.Location = new System.Drawing.Point(593, 266);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(193, 28);
             this.textBox9.TabIndex = 22;
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(153, 270);
+            this.textBox10.Location = new System.Drawing.Point(196, 313);
             this.textBox10.Name = "textBox10";
             this.textBox10.Size = new System.Drawing.Size(240, 28);
             this.textBox10.TabIndex = 23;
             // 
             // textBox11
             // 
-            this.textBox11.Location = new System.Drawing.Point(550, 270);
+            this.textBox11.Location = new System.Drawing.Point(593, 313);
             this.textBox11.Name = "textBox11";
             this.textBox11.Size = new System.Drawing.Size(193, 28);
             this.textBox11.TabIndex = 24;
@@ -267,7 +258,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(812, 529);
+            this.ClientSize = new System.Drawing.Size(915, 529);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox11);
             this.Controls.Add(this.textBox10);
@@ -280,7 +271,6 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -315,7 +305,6 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label12;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
--- CloseRelatives_Information.cs
+++ CloseRelatives_Information.cs
@@ -45,5 +45,132 @@ namespace Genealogy_Management_System
             th.Start();
             this.Close();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            string constr = "Server=.;Database=Genealogy Management System;Integrated Security=True";
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            //成员查找
+            string sql = "select * from Member where M_ID='" + textBox1.Text.Trim() + "' and G_ID=(select G_ID from Member where M_ID ='"+Globaldate.ID+"')";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            //判断成员ID是否存在以及是否是同族人员
+            if (!dr.Read())
+            {
+                MessageBox.Show("不存在该成员或者他不是本族人员！","提示");
+                textBox1.Text = "";
+            }
+
+            else
+            {
+
+                //显示相应事件信息
+                textBox2.Text = dr["M_Name"].ToString();
+                textBox3.Text = dr["M_Sex"].ToString();
+                textBox10.Text = dr["M_Birth"].ToString();
+                textBox9.Text = dr["M_Generation"].ToString();//字辈
+                textBox8.Text = dr["M_Ranking"].ToString();
+                textBox11.Text = dr["M_Birthplace"].ToString();
+
+                //保存当前登录用户的父母亲
+                string FatherID;
+                string MotherID;
+                string myfatherID;
+                string myMotherID;
+
+
+                dr.Close();
+                //母亲姓名查找
+                sql = "select M_Name from Member where M_ID=(select M_MotherID from Member where M_ID ='" + textBox1.Text + "')";
+                cmd = new SqlCommand(sql, con);
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    textBox5.Text = dr["M_Name"].ToString();
+                    MotherID = dr["M_ID"].ToString();
+                }
+                else
+                    MotherID = null;
+                dr.Close();
+                //父亲姓名查找
+                sql = "select M_Name from Member where M_ID=(select M_FatherID from Member where M_ID ='" + textBox1.Text + "')";
+                cmd = new SqlCommand(sql, con);
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    textBox4.Text = dr["M_Name"].ToString();
+                    FatherID = dr["M_ID"].ToString();
+                }
+                else
+                    FatherID = null;
+                dr.Close();
+                //配偶姓名查找
+                sql = "select M_Name from Member where M_ID=(select M_SpouseID from Member where M_ID ='" + textBox1.Text + "')";
+                cmd = new SqlCommand(sql, con);
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                    textBox4.Text = dr["M_Name"].ToString();
+                    
+
+                //显示近亲与自己的关系(两服)
+                string myID = Globaldate.ID;//myID是用户的ID，从登录界面读取
+
+                dr.Close();
+                sql = "select * from Member where M_ID='" + Globaldate.ID + "'";
+                cmd = new SqlCommand(sql, con);
+                dr = cmd.ExecuteReader();
+                if (!dr.Read())
+                    myfatherID = dr["M_FatherID"].ToString();//myfatherID是用户父亲的ID，
+                else
+                    myfatherID = null;
+
+                dr.Close();
+                sql = "select M_ID from Member where M_ID='" + Globaldate.ID + "'";
+                cmd = new SqlCommand(sql, con);
+                dr = cmd.ExecuteReader();
+                if (!dr.Read())
+                    myMotherID = dr["M_MotherID"].ToString();//mymatherID是用户母亲的ID，
+                else
+                    myMotherID = null;
+
+                dr.Close();
+                if (FatherID == myID || MotherID == myID)
+                    textBox7.Text = "他是我儿";
+                else if (FatherID == myfatherID|| myMotherID == MotherID && myID!=textBox1.Text)
+                {
+                    if(textBox3.Text=="女")
+                        textBox7.Text = "这是我姐妹儿";
+                    else
+                        textBox7.Text = "这是我兄弟";
+                }
+                else if (myID==MotherID)
+                {
+                    textBox7.Text = "我是他的母亲";
+                }
+                else if (textBox1.Text==myMotherID)
+                {
+                    textBox7.Text = "我的母亲";
+                }
+                else if (myID == FatherID)
+                {
+                    textBox7.Text = "我是他的父亲";
+                }
+                else if (textBox1.Text == myfatherID)
+                {
+                    textBox7.Text = "他是我爹";
+                }
+                else
+                {
+                    textBox7.Text = "系统君已晕，他是你的同宗！";
+                }
+
+
+
+
+            }
+        }
     }
 }
